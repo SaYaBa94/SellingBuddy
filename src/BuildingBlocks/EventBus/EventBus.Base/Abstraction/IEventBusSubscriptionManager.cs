47a50d5c4049782267301ac1c14c1d375f062f84
Yasin <sadriyasin.balkan@vericom.com.tr>
2024-05-18 20:23:07 +0300

    Add project files.

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBusSubscriptionManager.cs
@@ -0,0 +1,23 @@
+﻿using EventBus.Base.Events;
+using System;
+using System.Collections.Generic;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IEventBusSubscriptionManager
+    {
+        public bool IsEmpty { get; }
+
+        event EventHandler<string> OnEventRemoved;
+
+        void AddSubscription<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+        void RemoveSubscription<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+        bool HasSubscriptionsForEvent<T>() where T : IntegrationEvent;
+        bool HasSubscriptionsForEvent(string eventName);
+        Type GetEventTypeByName(string eventName);
+        void Clear();
+        IEnumerable<SubscriptionInfo> GetHandlerForEvent<T>() where T : IntegrationEvent;
+        IEnumerable<SubscriptionInfo> GetHandlerForEvent(string eventName); 
+        string GetEventKey<T>();
+    }
+}
